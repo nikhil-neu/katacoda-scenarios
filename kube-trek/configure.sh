ac05a9bc4eb94961385b66f0a081fa943c84edc3
nikhil-neu <nikhil.rajan67@gmail.com>
2021-09-15 19:54:17 +0530

    chnaes

--- kube-trek/configure.sh
+++ kube-trek/configure.sh
@@ -1,4 +1,5 @@
 launch.sh
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl create ns species
 kubectl apply -f /opt/species/
 rm -rf /opt/species
