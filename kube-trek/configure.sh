a304ac386b9ce2afb4dea5b27146d9b2d73d648d
nikhil-neu <nikhil.rajan67@gmail.com>
2021-09-16 14:26:27 +0530

    changes

--- kube-trek/configure.sh
+++ kube-trek/configure.sh
@@ -1,11 +1,3 @@
 launch.sh
-sleep 100
-kubectl create ns species
-kubectl create ns annika
-kubectl apply -f /opt/AnnikaSql/
-kubectl apply -f /opt/simpleAnnika/
-kubectl apply -f /opt/Species
-
-
 echo "Started" 
 echo 'done' > /opt/katacoda-background-finished
--- kube-trek/foreground.sh
+++ kube-trek/foreground.sh
@@ -1,1 +1,7 @@
 launch.sh
+sleep 60
+kubectl create ns species
+kubectl create ns annika
+kubectl apply -f /opt/AnnikaSql/
+kubectl apply -f /opt/simpleAnnika/
+kubectl apply -f /opt/Species
