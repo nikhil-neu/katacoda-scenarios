b57e17b50afcd1b499af3c0a913cf6174e7495bf
nikhil-neu <nikhil.rajan67@gmail.com>
2021-09-17 02:27:48 +0530

    aaa

--- kube-trek/4_verify.sh
+++ kube-trek/4_verify.sh
@@ -1,2 +1,1 @@
-var1=$(kubectl get ep annika-service -n annika --no-headers|xargs|cut -f2 -d ' ')|var2=$(kubectl describe pod annika -n annika|grep 'IP:'| head -1|xargs | cut -f2 -d ' '):8080|var3=$(kubectl get svc annika-service -n annika | grep 30080)|if [[ $var1==$var2 && ! -z "$var3" ]]; then echo "good";fi|grep good
-
+var1="a"| var2="a"|if [ $a==$b ];then echo "good";fi | grep good
