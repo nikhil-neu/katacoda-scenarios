4efe9c48978acc44ff8973d6d44ce918304d7d11
nikhil-neu <nikhil.rajan67@gmail.com>
2021-09-17 13:30:05 +0530

    changes

--- kube-trek/3_verify.sh
+++ kube-trek/3_verify.sh
@@ -1,1 +1,1 @@
-kubectl top pods -n kube-system --sort-by=memory --no-headers=true | cut -f1 -d ' '| tr "\n" "," |grep 'vulcans,humans,klingons,'
+kubectl top pods -n species --sort-by=memory --no-headers=true | cut -f1 -d ' '| tr "\n" "," |grep 'vulcans,humans,klingons,'
