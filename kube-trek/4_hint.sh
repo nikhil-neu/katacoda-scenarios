a25f673f40c89cbe492154207f59ba3d5a5ed344
nikhil-neu <nikhil.rajan67@gmail.com>
2021-09-19 00:46:36 +0530

    changes

--- kube-trek/4_hint.sh
+++ kube-trek/4_hint.sh
@@ -7,6 +7,7 @@ fi
 
 if [[ $seconds_sofar -ge 90 &&  $seconds_sofar -lt 180 ]]; then
   echo "Keep going, a hint will be shown soon..."
+  echo "Ensure all the ports and selectors and correct"
   echo "Ensure the AnnikaWeb tab loads without error"
 fi
 
@@ -14,5 +15,6 @@ fi
 if [ $seconds_sofar -ge 180 ]; then
   echo "Feel free to modify any of the pod specs"
   echo "answer will be validated automatically"
+  echo "Ensure all the ports and selectors and correct"
   echo "Ensure the AnnikaWeb tab loads without error"
 fi
--- kube-trek/5_hint.sh
+++ kube-trek/5_hint.sh
@@ -2,12 +2,14 @@ seconds_sofar=$1
 
 if [[ $seconds_sofar -ge 90 &&  $seconds_sofar -lt 180 ]]; then
   echo "Keep going, a hint will be shown soon..."
+  echo "She was a freindly Borg"
   echo "Ensure the AnnikaSqlWeb tab loads without error"
 fi
 
 
 if [ $seconds_sofar -ge 180 ]; then
   echo "Feel free to modify any of the pod specs"
+  echo "Kubernetes code name was named after Her"
   echo "Ensure the AnnikaSqlWeb tab loads without error"
   echo "answer will be validated automatically"
 fi
